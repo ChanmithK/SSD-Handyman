76609f6193c9c5ef7e6b0a271221e1b6542af7f3
Praveen Kariyawasam <praveenkariyawasam0@gmail.com>
2023-09-26 22:13:26 +0530

    Application hosted

--- src/Context/UserAuthContext.js
+++ src/Context/UserAuthContext.js
@@ -8,14 +8,20 @@ import {
   GoogleAuthProvider,
   signInWithPopup,
 } from "firebase/auth";
-import { auth } from "../firebase-config";
+import { auth, db } from "../firebase-config";
 import Cookies from "js-cookie";
+import { useDispatch } from "react-redux";
+import { setUserData } from "../redux/UserDataSlice";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 const userAuthContext = createContext();
 
 export function UserAuthContextProvider({ children }) {
+  const dispatch = useDispatch();
+  const [userDetails, setUserDetails] = useState({});
+  const usersCollectionRef = collection(db, "users");
+
   const [user, setUser] = useState({});
-  // console.log('this is user', user.uid ? user.uid : null)
   function signUp(email, password) {
     return createUserWithEmailAndPassword(auth, email, password);
   }
@@ -32,31 +38,108 @@ export function UserAuthContextProvider({ children }) {
     return signOut(auth);
   };
 
+  //Broken Authentication Flaws - Praveen
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
   };
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
-      setUser(currentuser);
+  //Broken Authentication Flaws - Praveen
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
+  //     setUser(currentuser);
 
-      const token = currentuser.accessToken;
+  //     const token = currentuser.accessToken;
+  //     const tokenOptions = {
+  //       secure: true,
+  //       httpOnly: true,
+  //       sameSite: "strict",
+  //       expires: 1,
+  //     };
+  //     Cookies.set("token", token, tokenOptions);
+
+  //     const currectUser = {
+  //       username: currentuser.displayName,
+  //       userImage: currentuser.photoURL,
+  //       userId: currentuser.uid,
+  //       email: currentuser.email,
+  //     };
+  //     dispatch(setUserData(currectUser));
+
+  //     if (currentuser.displayName === null) {
+  //       const getUser = async () => {
+  //         const filterdData = query(
+  //           usersCollectionRef,
+  //           where("user", "==", user.uid)
+  //         );
+  //         const querySnapshot = await getDocs(filterdData);
+  //         setUserDetails(
+  //           querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+  //         );
+  //       };
+  //       getUser().then(dispatch(setUserData(userDetails)));
+
+  //       console.log("This is NEW user DATA", userDetails);
+  //     }
+  //   });
+
+  //   return () => {
+  //     unsubscribe();
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      setUser(currentUser);
+
+      const token = currentUser.accessToken;
       const tokenOptions = {
-        secure: true, // Transmit only over HTTPS
-        httpOnly: true, // Prevent client-side JavaScript access
-        sameSite: "strict", // Prevent cross-site request forgery (CSRF)
-        expires: 1, // Set an appropriate expiration (e.g., 1 day)
+        secure: true,
+        httpOnly: true,
+        sameSite: "strict",
+        expires: 1,
+      };
+      Cookies.set("token", token, tokenOptions);
+
+      const currentUserData = {
+        username: currentUser.displayName,
+        userImage: currentUser.photoURL,
+        userId: currentUser.uid,
+        email: currentUser.email,
       };
 
-      Cookies.set("token", token, tokenOptions);
-      // localStorage.setItem("token", currentuser.accessToken);
+      dispatch(setUserData(currentUserData));
+
+      if (currentUser.displayName === null) {
+        try {
+          const filteredData = query(
+            usersCollectionRef,
+            where("user", "==", currentUser.uid)
+          );
+          const querySnapshot = await getDocs(filteredData);
+          const userDetailsData = querySnapshot.docs.map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+          }));
+
+          setUserDetails(userDetailsData);
+          dispatch(setUserData(userDetailsData));
+
+          console.log("This is NEW user DATA", userDetailsData);
+        } catch (error) {
+          console.error("Error fetching user details:", error);
+        }
+      }
     });
 
     return () => {
       unsubscribe();
     };
   }, []);
+
+  // useEffect(() => {
+
+  // }, []);
 
   return (
     <userAuthContext.Provider
